8a53825e1cb14a9b48a40754efde708a41089ad1
Oleg Nesterov <oleg9988@rambler.ru>
2024-03-20 12:50:00 +0300

    Перевод Repository на хранение в SQLite

--- app/src/main/java/ru/netology/nmedia/db/AppDB.kt
+++ app/src/main/java/ru/netology/nmedia/db/AppDB.kt
@@ -1,44 +1,29 @@
 package ru.netology.nmedia.db
 
 import android.content.Context
-import android.database.sqlite.SQLiteDatabase
-import android.database.sqlite.SQLiteOpenHelper
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
 import ru.netology.nmedia.dao.PostDAO
-import ru.netology.nmedia.dao.PostDAOImpl
+import ru.netology.nmedia.entity.PostEntity
+import ru.netology.nmedia.entity.RepoEntity
 
-class AppDB private constructor(db: SQLiteDatabase) {
-    val postDAO: PostDAO = PostDAOImpl(db)
-
+@Database(entities = [PostEntity::class, RepoEntity::class], version = 1)
+abstract class AppDB :RoomDatabase() {
+    abstract fun postDAO() :PostDAO
     companion object {
         @Volatile
         private var instance: AppDB? = null
 
         fun getInstance(context: Context): AppDB {
             return instance ?: synchronized(this) {
-                instance ?: AppDB(
-                    buildDatabase(context, arrayOf(PostDAOImpl.DDL, PostDAOImpl.DDLRepo))
-                ).also { instance = it }
+                instance ?: buildDatabase(context).also { instance = it }
             }
         }
 
-        private fun buildDatabase(context: Context, DDLs: Array<String>) =
-            DbHelper(context, 1, "app.db", DDLs).writableDatabase
-    }
-}
-
-class DbHelper(context: Context, dbVersion: Int, dbName: String, private val DDLs: Array<String>) :
-    SQLiteOpenHelper(context, dbName, null, dbVersion) {
-    override fun onCreate(db: SQLiteDatabase) {
-        DDLs.forEach {
-            db.execSQL(it)
-        }
-    }
-
-    override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
-        TODO("Not implemented")
-    }
-
-    override fun onDowngrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
-        TODO("Not implemented")
+        private fun buildDatabase(context: Context) =
+            Room.databaseBuilder(context, AppDB::class.java, "app.db")
+                .allowMainThreadQueries()
+                .build()
     }
 }
